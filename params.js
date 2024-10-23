8916d1c732ed3847b74dc862c27110c8af6331f6
Kathryn Schuler <kathryn.schuler@gmail.com>
2024-10-23 16:27:57 -0400

    add stims and all tasks

---
+++ params.js
@@ -0,0 +1,37 @@
+var PARAMS = {
+  preload : {
+    audio: [
+      "sounds/bikuti.wav"
+    ], 
+    videos: [
+      "video/Pingu_Block1.mp4"
+    ],
+    images: []
+  }, 
+  conditions : {
+    A: {
+      name: "2AFC",
+      instructions: "Certain combinations of sounds ten",
+      test: {
+        A: {},
+        B: {}
+      }
+    }, 
+    B: {
+      name: "RATING",
+      instructions: "here",
+      test: {
+        A: {},
+        B: {}
+      }
+    }, 
+    C: {
+      name: "SICR",
+      instructions: "here", 
+      test: {
+        A: {},
+        B: {}
+      }
+    }
+  }
+}
