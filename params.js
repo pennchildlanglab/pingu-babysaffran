c21cb1e3ac090d2299fd6e5d444794fd0da1e96f
Kathryn Schuler <kathryn.schuler@gmail.com>
2024-11-09 12:31:55 -0500

    update

--- params.js
+++ params.js
@@ -1,36 +1,80 @@
 var PARAMS = {
   preload : {
     audio: [
-      "sounds/bikuti.wav"
+      "sounds/bikuti-resampled.wav",
+      "sounds/budopa-resampled.wav",
+      "sounds/daropi-resampled.wav",
+      "sounds/golatu-resampled.wav",
+      "sounds/kudabi-resampled.wav",
+      "sounds/pabiku-resampled.wav",
+      "sounds/pigola-resampled.wav",      
+      "sounds/tibudo-resampled.wav",
+      "sounds/tigobu-resampled.wav",
+      "sounds/tudaro-resampled.wav",
+      "sounds/exposure_1min30sec.wav"
     ], 
     videos: [
-      "video/Pingu_Block1.mp4"
+      "video/Pingu_Block1.mp4",
+      "video/Pingu_Block2.mp4",
+      "video/Pingu_Block3.mp4"
     ],
-    images: []
+    images: [
+      "images/pingu-still-wave.png"
+    ]
   }, 
   conditions : {
-    A: {
-      name: "2AFC",
-      instructions: "Certain combinations of sounds ten",
+    AFC : {
+      name: "AFC",
+      instructions: "AFC instructions here.",
       test: {
-        A: {},
-        B: {}
-      }
+        list1: [
+          {sound1: "sounds/bikuti-resampled.wav", sound2: "sounds/daropi-resampled.wav" }, 
+          {sound1: "sounds/budopa-resampled.wav", sound2: "sounds/golatu-resampled.wav"},
+          {sound1: "sounds/pabiku-resampled.wav", sound2: "sounds/tudaro-resampled.wav"}, 
+          {sound1: "sounds/daropi-resampled.wav", sound2: "sounds/budopa-resampled.wav"}
+        ],
+        list2: [
+          {sound1: "sounds/bikuti-resampled.wav", sound2: "sounds/daropi-resampled.wav" }, 
+          {sound1: "sounds/budopa-resampled.wav", sound2: "sounds/golatu-resampled.wav"},
+          {sound1: "sounds/pabiku-resampled.wav", sound2: "sounds/tudaro-resampled.wav"}, 
+          {sound1: "sounds/daropi-resampled.wav", sound2: "sounds/budopa-resampled.wav"}
+        ]
+     }
     }, 
-    B: {
+    RATING: {
       name: "RATING",
-      instructions: "here",
+      instructions: "Rating instructions here.",
       test: {
-        A: {},
-        B: {}
+        list1: [
+          {sound1: "sounds/bikuti-resampled.wav"}, 
+          {sound1: "sounds/daropi-resampled.wav"},
+          {sound1: "sounds/budopa-resampled.wav"}, 
+          {sound1: "sounds/golatu-resampled.wav"}
+        ],
+        list2: [
+          {sound1: "sounds/bikuti-resampled.wav"}, 
+          {sound1: "sounds/daropi-resampled.wav"},
+          {sound1: "sounds/budopa-resampled.wav"}, 
+          {sound1: "sounds/golatu-resampled.wav"}
+        ]
       }
     }, 
-    C: {
+    SICR: {
       name: "SICR",
-      instructions: "here", 
+      instructions: "SICR instructions here.", 
       test: {
-        A: {},
-        B: {}
+        list1: [
+          {sound1: "sounds/bikuti-resampled.wav", sound2: "sounds/daropi-resampled.wav" }, 
+              {sound1: "sounds/budopa-resampled.wav", sound2: "sounds/golatu-resampled.wav"},
+              {sound1: "sounds/pabiku-resampled.wav", sound2: "sounds/tudaro-resampled.wav"}, 
+              {sound1: "sounds/daropi-resampled.wav", sound2: "sounds/budopa-resampled.wav"}
+          ],
+        list2: [
+          {sound1: "sounds/bikuti-resampled.wav", sound2: "sounds/daropi-resampled.wav" }, 
+              {sound1: "sounds/budopa-resampled.wav", sound2: "sounds/golatu-resampled.wav"},
+              {sound1: "sounds/pabiku-resampled.wav", sound2: "sounds/tudaro-resampled.wav"}, 
+              {sound1: "sounds/daropi-resampled.wav", sound2: "sounds/budopa-resampled.wav"}
+          ]
       }
     }
   }
